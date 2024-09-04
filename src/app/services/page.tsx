b1c121de4a00b69123d0a72785dcb2cc1ff60d6a
romeucajamba <romeucajamba@gmail.com>
2024-09-04 11:20:39 +0100

    feat:add services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -59,14 +59,18 @@ export default function Services(){
                   return (
                      <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                         <div className="w-full flex justify-between items-center">
-                            <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">{services.num}</div>
-                            <Link href={services.href}>
-                                <BsArrowRight/>
+                            <div className="text-5xl font-extrabold text-outline text-transparent 
+                              group-hover:text-outline-hover 
+                              transation-all duration-500">{services.num}</div>
+                            <Link href={services.href} className='w-[70px] h-[70px] rounded-full bg-white 
+                              group-hover:bg-accent transation-all duration-500 flex justify-center items-center
+                                hover:-rotate-45 group-hover:cursor-pointer'>
+                                <BsArrowRight className='text-primary text-3xl'/>
                             </Link>
                         </div>
 
-                        <h2>{services.title}</h2>
-                        <p>{services.description}</p>
+                        <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>{services.title}</h2>
+                        <p className='text-white/60'>{services.description}</p>
                         <div className="border-b border-white/20 w-full"></div>
                      </div>
                   )
